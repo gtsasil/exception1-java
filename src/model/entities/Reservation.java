5a21a8b62baa088ebabf5013a5906f2f994c20fb
Gleisson Santos Silva <gtsasil@gmail.com>
2018-09-24 13:20:56 +0100

    Final code(GoodOne)

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 	 
 	private Integer roomNumber;
@@ -12,8 +14,11 @@ public class Reservation {
 	
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
 	
-	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
-
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut){
+		
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -40,19 +45,18 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut){
 		Date now = new Date();
 		if(checkIn.before(now)  || checkOut.before(now)) {
-			return "Error in reservation: Reservation dates for update must be future.";
+			throw new DomainException("Reservation dates for update must be future.");
 		}
 		if (!checkOut.after(checkIn)) {
-			return "Error in reservation: Reservation dates for update must be future.";
+			throw new DomainException("Check-out date must be after check-in date");
 		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
-	}
-	
+		
+	}	
 	@Override
 	public String toString() {
 		return "Room "
